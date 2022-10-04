ec4d4c36c234ab0b1a5044d8c2dda07b24cf00df
Angelo <angelomikael407@gmail.com>
2022-10-04 18:08:02 +0200

    latest

--- srcs/execution/fc_exe_with_re.c
+++ srcs/execution/fc_exe_with_re.c
@@ -6,7 +6,7 @@
 /*   By: angelo <marvin@42lausanne.ch>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 12:44:38 by angelo            #+#    #+#             */
-/*   Updated: 2022/10/04 17:47:57 by angelo           ###   ########.fr       */
+/*   Updated: 2022/10/04 18:05:44 by angelo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ static int	fc_init_exe_with_re(t_info *info)
 int	fc_exe_with_re(t_info *info)
 {
 	int	i;
-	int	j;
+//	int	j;
 
 	fc_init_exe_with_re(info);
 	i = 0;
@@ -34,8 +34,6 @@ int	fc_exe_with_re(t_info *info)
 		info->exe->pid_lst[i] = fork();
 		if (info->exe->pid_lst[i] < 0)
 			fc_error_tmp(1, "Problem with fork - info->exe->pid_lst[i]\n");
-		//printf("i = %d\n", i);
-		
 		while (info->exe->pid_lst[i] == 0)
 		{
 			info->idx = 0;
@@ -56,14 +54,17 @@ int	fc_exe_with_re(t_info *info)
 	if (close(info->exe->fd[1]) < 0)
 		fc_error_tmp(1, "Problem with close(info->exe->fd[1])\n");
 
-	j = 0;
-	while (j < info->lex->nbr_pipe + 1)
-	{
+	//j = 0;
+	//while (j < info->lex->nbr_pipe + 1)
+	//{
 		info->exe->w_pid = waitpid(info->exe->pid_lst[i], WIFEXITED(true), 0);
 		if (info->exe->w_pid < 0)
 			fc_error_tmp(1, "Problem with waitpid - info->exe->w_pid\n");
-		j++;
-	}
+		if (info->exe->w_pid  > -1)
+			printf("SUCCESS, i = %d\n", i);
+		
+	//	j++;
+	//}
 
 	return (0);
 }
